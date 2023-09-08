edb29dd1897c2e80efd2a5ce392a970d63e5d472
Space Nerd <florianpospiech77@gmail.com>
2023-09-08 22:30:29 +0200

    Basic Character render on screen and window configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use bevy::prelude::*;
+
+pub const SCREEN_WIDTH: f32 = 800.0;
+pub const SCREEN_HEIGHT: f32 = 800.0;
+
+#[derive(Component)]
+struct Player;
+
 fn main() {
-    println!("Test!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Jump and Run".into(),
+                resolution: (SCREEN_WIDTH, SCREEN_HEIGHT).into(),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_startup_systems(setup_camera)
+        .add_startup_systems(setup_player)
+        .run();
+}
+
+fn setup_camera(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn setup_player(
+    mut commands: Commands,
+    mut texture_atlas: ResMut<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>,
+) {
+    let atlas = TextureAtlas::from_grid(
+        asset_server.load("sprites/AnimationSheet_Character.png"),
+        Vec2::splat(32.),
+        8, 9, None, None,
+    );
+
+    commands.spawn((SpriteSheetBundle {
+        texture_atlas: texture_atlas.add(atlas),
+        sprite: TextureAtlasSprite {index: 0, ..Default::default()},
+        ..Default::default()
+    }, Player));
 }
